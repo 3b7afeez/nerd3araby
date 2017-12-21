9eceb05e98cb6eed5a41f105bcb30b94e49923aa
3b7afeez <3b7afeez@gmail.com>
2017-12-21 06:24:55 +0200

    first commit

---
+++ src/movies/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+app_name = 'movies'
+
+# urlpatterns = [
+#     path('books/', include('books.urls')),
+#     path('movies/', include('movies.urls')),
+#     path('music/', include('music.urls')),
+# ]
