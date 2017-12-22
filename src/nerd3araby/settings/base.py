2e71fbc9c4ed0327b962842b65a5e5c739eebcdf
3b7afeez <3b7afeez@gmail.com>
2017-12-22 09:57:52 +0200

    stablizing static files

--- src/nerd3araby/settings/base.py
+++ src/nerd3araby/settings/base.py
@@ -124,7 +124,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    # '/var/www/static/',
+]
 
 CORS_REPLACE_HTTPS_REFERER      = False
 HOST_SCHEME                     = "http://"
